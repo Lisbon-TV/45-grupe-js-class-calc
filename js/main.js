de2ad40a918794885c4ba7e5d4e415aad8f119cd
Lisbon <lisbon@dublin.com>
2023-06-21 12:38:44 +0300

    Upddate

--- js/main.js
+++ js/main.js
@@ -39,6 +39,73 @@ calc.innerHTML
 `<div class="key color">&lt;=</div>`
 `<div class="key color">+</div>`
 `<div class="key color height-2">=</div>`
+
+
+
+
+ `<div class="calc">
+        <div class="screen">
+            <div class="equation"></div>
+            <div class="result">0</div>
+        </div>'
+
+`        <div class="keyboard">
+            <div class="key">C</div>
+            <div class="key">/</div>
+            <div class="key">*</div>
+            <div class="key color">&lt;=</div>
+
+            <div class="key">7</div>
+            <div class="key">8</div>
+            <div class="key">9</div>
+            <div class="key color">-</div>
+
+            <div class="key">4</div>
+            <div class="key">5</div>
+            <div class="key">6</div>
+            <div class="key color">+</div>
+
+            <div class="key">1</div>
+            <div class="key">2</div>
+            <div class="key">3</div>
+            <div class="key color height-2">=</div>
+
+            <div class="key">.</div>
+            <div class="key">0</div>
+            <div class="key">%</div>
+        </div>`
+
+Vaido: 
+let formDOM = document.querySelector("form");
+let numberDOM = formDOM.querySelector("div");
+let b1DOM = formDOM.querySelector("button");
+let b2DOM = formDOM.querySelector("button:nth-child(3)");
+let b3DOM = formDOM.querySelector("button:nth-child(4)");
+let b4DOM = formDOM.querySelector("button:nth-child(5)");
+let b5DOM = formDOM.querySelector("button:nth-child(6)");
+let b6DOM = formDOM.querySelector("button:nth-child(7)");
+let b7DOM = formDOM.querySelector("button:nth-child(8)");
+let b8DOM = formDOM.querySelector("button:nth-child(9)");
+let b9DOM = formDOM.querySelector("button:nth-child(10)");
+let b10DOM = formDOM.querySelector("button:nth-child(11)");
+let b11DOM = formDOM.querySelector("button:nth-child(12)");
+let b12DOM = formDOM.querySelector("button:nth-child(13)");
+let b13DOM = formDOM.querySelector("button:nth-child(14)");
+let b14DOM = formDOM.querySelector("button:nth-child(15)");
+let b15DOM = formDOM.querySelector("button:nth-child(16)");
+let b16DOM = formDOM.querySelector("button:nth-child(17)");
+let b17DOM = formDOM.querySelector("button:nth-child(18)");
+
+let counter = 0;
+let counterP = 0;
+let counterM = 0;
+let counterDG = 0;
+let counterDL = 0;
+let counterS = 0;
+let counterB = 0;
+let counterC = 0;
+
+pasiziureti Vaido koda!
 */
 
 
